763858438bc4a1b083c8c513bbc4f964118561d6
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-11-03 22:21:52 +1000

    removed uselessnessessess

--- Sandbox/mc_deck.ts
+++
@@ -1,9 +0,0 @@
-import { kill, say, tag } from 'sandstone/commands'
-import { mcfunction, saveDatapack } from 'sandstone/core'
-
-mcfunction('setup', () => {
-  say('Setting up Mc Deck data')
-  kill('@e')
-})
-
-saveDatapack('Mc Deck', { verbose: true })
